d56a3d1f5ad647376c3abbe27f0763de7af24dfb
Simon Mairinger <simon@MacBookPro.lan>
2025-10-13 15:19:00 +0200

    test stage

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-50">
     <h1 class="text-4xl font-bold text-brand-600 underline">
-      Hello Tailwind + Nuxt 4 🎉
+      Hello Vercel-Stagesystem!
     </h1>
   </div>
 </template>
