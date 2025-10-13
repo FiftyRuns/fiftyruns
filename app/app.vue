c77b2a277506681f683fcb8d5cc4e122f78d9774
Simon Mairinger <simon.mairinger@cyberhouse.at>
2025-10-13 17:08:40 +0200

    Test commit mit korrekter Atlassian-E-Mail

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-50">
     <h1 class="text-4xl font-bold text-brand-600 underline">
-      Hello Vercel-Stagesystem!
+      Hello Vercel-Stagesystem, nun ist die E-Mail Adresse korrekt!
     </h1>
   </div>
 </template>
